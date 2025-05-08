f4c276dafed209feb2ca5710c5023903ca35c3e8
Chayanon Boonthanom <166270677+Chayanonboo@users.noreply.github.com>
2025-05-08 14:24:33 +0700

    Update Chatbot ordering Pizza.r

--- Project/Chatbot ordering Pizza.r
+++ Project/Chatbot ordering Pizza.r
@@ -1,6 +1,35 @@
-## homework 
-## สามารถเริ่มการใช้ Program Chatbot ordering Pizza ได้ด้วยการกดฟังก์ชัน pizza()
-
+## homework
+## ขั้นตอนในการใช้โปรแกรมนี้
+## 1. สามารถเริ่มการใช้ Program Chatbot ordering Pizza ได้ด้วยการกดฟังก์ชัน pizza()
+## 2. จะมีประโยคขึ้นมาดังนี้ 
+pizza()
+[1] "Hello"                                      
+[2] "My name is Non,welcome to pizza restaurant!"
+How can I help you?
+ให้ตอบ yes เพื่อไปขั้นตอนถัดไป
+## 3 ให้เลือก crust Pizza (Thin, Pan, Sausage, Extreme-cheese) 4 crust ดังนี้
+[1] "Select your crust" เลือก Pan
+  crust_id     crust_name price
+1        1           Thin    20
+2        2            Pan    25
+3        3        Sausage    30
+4        4 Extreme-cheese    35
+## 4 ให้เลือกหน้า Topping (Hawaiian, Cocktail Shrimp, Seafood Deluxe, Chicken Spicy, Tom Yum Kung) 5 Topping ดังนี้
+[1] "Select your topping" เลือก Hawaiian
+  topping_id    topping_name price
+1          1        Hawaiian   250
+2          2 Cocktail Shrimp   260
+3          3  Seafood Deluxe   270
+4          4   Chicken Spicy   280
+5          5    Tom Yum Kung   290
+## 5 มีเจอ Anything else ..... ให้ตอบ yes
+## 6 ให้เลือกหน้า Other(Drinking Water, Chicken Nuggets, BBQ Chickern Wings) 3 Other ดังนี้
+เลือก Drinking Water
+  other_id         other_name price
+1        1     Drinking Water    42
+2        2    Chicken Nuggets    40
+3        3 BBQ Chickern Wings    78
+## 7 สุดท้ายจะแสดงผลลัพธ์ออกมาดังนี้ [1] "The total price is 317 $"
 
 ## 1. chatbot order Pizza 
 install.packages("glue")
@@ -79,10 +108,10 @@ pizza <- function() {
     } 
 
     if(select_other %in% list_other$other_name) {
-      price_c <- list_other[which(list_other$other_name == select_other), "price"]
+      price_o <- list_other[which(list_other$other_name == select_other), "price"]
     } 
 
-    print(paste0("The total price is ", price_c + price_t + price_c, " $"))
+    print(paste0("The total price is ", price_c + price_t + price_o, " $"))
   } else {
     print("Thank for your serving")
   }
